2147a92f13506834e42395b2d247417f1918cf66
vbhavana <bhavanavakkalagadda@gmail.com>
2020-02-20 22:54:30 -0700

    calculator

---
+++ Assets/calcu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class calcu : MonoBehaviour
+{
+    // inputfield input
+    public InputField firstnumber, secondnumber;
+    public Text ans;
+    private int fnum;
+    private int snum;
+    public void onAdd()
+    {
+        int fnum = int.Parse(firstnumber.text);
+        int snum = int.Parse(secondnumber.text);
+        ans.text = "Result "+" "+(fnum + snum).ToString();
+    }
+    public void onSub()
+    {
+        int fnum = int.Parse(firstnumber.text);
+        int snum = int.Parse(secondnumber.text);
+        ans.text = "Result "+" "+(fnum -  snum).ToString();
+    }
+    /*  // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } */
+}
